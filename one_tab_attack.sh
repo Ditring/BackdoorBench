a2fde584f740045a30ecfc99be20af175b55ccdc
yuhongrui <yuhongrui@buaa.edu.cn>
2025-07-08 00:33:41 +0800

    上传一键生成攻击数据集的代码

---
+++ one_tab_attack.sh
@@ -0,0 +1,31 @@
+#cifar10
+python ./attack/badnet.py --yaml_path "./config/attack/prototype/cifar10.yaml" --save_folder_name "badnet_cifar10_colorful_suqare_0.1pratio"  --attack badnet --dataset cifar10 --pratio 0.1 --patch_mask_path ./resource/badnet/colorful_suqare.png
+python ./attack/badnet.py --yaml_path "./config/attack/prototype/cifar10.yaml" --save_folder_name "badnet_cifar10_white_square_0.1pratio"  --attack badnet --dataset cifar10 --pratio 0.1 --patch_mask_path ./resource/badnet/white_square.png
+
+python ./attack/blended.py --yaml_path "./config/attack/prototype/cifar10.yaml" --save_folder_name "blended_cifar10_hello_kitty_0.1pratio"  --attack blended --dataset cifar10 --pratio 0.1 --patch_mask_path ./resource/blended/hello_kitty.jpeg
+
+python ./attack/ssba.py --yaml_path "./config/attack/prototype/cifar10.yaml" --save_folder_name "ssba_cifar10_0.1pratio"  --attack ssba --dataset cifar10 --pratio 0.1
+
+python ./attack/sig.py --yaml_path "./config/attack/prototype/cifar10.yaml" --save_folder_name "sig_cifar10_0.04pratio"  --attack sig --dataset cifar10 --pratio 0.1
+
+python ./attack/lc.py --yaml_path "./config/attack/prototype/cifar10.yaml" --save_folder_name "lc_cifar10_0.04pratio"  --attack lc --dataset cifar10 --pratio 0.04
+
+python ./attack/inputaware.py --yaml_path "./config/attack/prototype/cifar10.yaml" --save_folder_name "iab_cifar10_0.04pratio"  --attack inputaware --dataset cifar10 --pratio 0.04
+
+
+#tiny
+python ./attack/badnet.py --yaml_path "./config/attack/prototype/tiny.yaml" --save_folder_name "badnet_tiny_colorful_suqare_0.1pratio"  --attack badnet --dataset tiny --pratio 0.1 --patch_mask_path ./resource/badnet/colorful_suqare.png
+python ./attack/badnet.py --yaml_path "./config/attack/prototype/tiny.yaml" --save_folder_name "badnet_tiny_white_square_0.1pratio"  --attack badnet --dataset tiny --pratio 0.1 --patch_mask_path ./resource/badnet/white_square.png
+
+python ./attack/blended.py --yaml_path "./config/attack/prototype/tiny.yaml" --save_folder_name "blended_tiny_hello_kitty_0.1pratio"  --attack blended --dataset tiny --pratio 0.1 --patch_mask_path ./resource/blended/hello_kitty.jpeg
+
+python ./attack/ssba.py --yaml_path "./config/attack/prototype/tiny.yaml" --save_folder_name "ssba_tiny_0.1pratio"  --attack ssba --dataset tiny --pratio 0.1
+
+python ./attack/inputaware.py --yaml_path "./config/attack/prototype/tiny.yaml" --save_folder_name "iab_tiny_0.04pratio"  --attack inputaware --dataset tiny --pratio 0.1
+
+python ./attack/wanet.py --yaml_path "./config/attack/prototype/tiny.yaml" --save_folder_name "wanet_tiny_0.04pratio"  --attack wanet --dataset tiny --pratio 0.1
+
+python ./attack/sig.py --yaml_path "./config/attack/prototype/tiny.yaml" --save_folder_name "sig_tiny_0.004pratio"  --attack sig --dataset tiny --pratio 0.004
+
+python ./attack/lc.py --yaml_path "./config/attack/prototype/tiny.yaml" --save_folder_name "lc_tiny_0.004pratio"  --attack lc --dataset tiny --pratio 0.004
+
